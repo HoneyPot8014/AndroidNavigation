5a59a589b36357577924f1f8b3aaa12424f7e02f
leeyh <gch02310@naver.com>
2019-07-24 17:24:33 +0900

    add drawable function

--- app/src/main/java/com/lyh/androidnavigation/MainActivity.kt
+++ app/src/main/java/com/lyh/androidnavigation/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.lyh.androidnavigation
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(tb_main)
+
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.firstFragment),
+                dl_main ?: return)
 
         val navHostFragment: NavHostFragment = supportFragmentManager
                 .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
@@ -26,11 +32,17 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         setUpBottomNavMenu(navController)
+        setUpNavMenu(navController)
+
     }
 
     private fun setUpBottomNavMenu(navController: NavController) {
         bottom_nav_view?.setupWithNavController(navController)
         nav_view?.setupWithNavController(navController)
+    }
+
+    private fun setUpNavMenu(navController: NavController) {
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -39,13 +51,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(bottom_nav_view != null) {
-            Log.d("bottomNav", "!null")
-        } else {
-            Log.d("bottomNav", "null")
-        }
+        dl_main?.openDrawer(GravityCompat.START)
         return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
                 || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.my_nav_host_fragment).navigateUp()
     }
 
 }
