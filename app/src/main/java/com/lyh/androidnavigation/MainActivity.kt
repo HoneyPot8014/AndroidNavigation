5f5617b46bafc135303be1e30942c0e4004cc82c
leeyh <gch02310@naver.com>
2019-07-24 15:45:25 +0900

    add Menu function
    when click menu, change fragment

--- app/src/main/java/com/lyh/androidnavigation/MainActivity.kt
+++ app/src/main/java/com/lyh/androidnavigation/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.lyh.androidnavigation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,13 @@ class MainActivity : AppCompatActivity() {
                 .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
 
         val navController = navHostFragment.navController
+
+        setUpBottomNavMenu(navController)
+    }
+
+    private fun setUpBottomNavMenu(navController: NavController) {
+        bottom_nav_view?.setupWithNavController(navController)
+        nav_view?.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -29,7 +39,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(bottom_nav_view != null) {
+            Log.d("bottomNav", "!null")
+        } else {
+            Log.d("bottomNav", "null")
+        }
         return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
                 || super.onOptionsItemSelected(item)
     }
+
 }
